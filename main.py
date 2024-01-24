5fa64aab3fac539a5faa6b45ac8315785341217b
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-01-24 12:23:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ api_secret = "xVM8dF8qIhTRtfaTShbHON7oJffooUbP2wp3oPqYUbFLJ1ZCHLN9dEmN9niAYzVF"
 client = Client(api_key, api_secret)
 exchange_info = client.get_exchange_info()
 
-futures_trading_pairs = [s['symbol'] for s in exchange_info['symbols'] if s['contractType'] == 'PERPETUAL']
-
+futures_trading_pairs = [s['symbol'] for s in exchange_info['symbols']
 for trading_pair in futures_trading_pairs:
     print(trading_pair)
